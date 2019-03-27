f4541e960cb256f24ab0c60a1edb94cdebcfbbfa
hoangluc98 <luclego98@gmail.com>
2019-03-27 13:15:27 +0700

    Giao dien trang chu

---
+++ fate_in_love/resources/views/trangchu.blade.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>CSS Template</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<base href="{{asset('')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/page.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/my.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/profile.css')}}">
+</head>
+<body style="background-image: url('image/lovebg.png');">
+	<div class="header" style="background-image: url('image/header.jpg');">
+		<a href="#default" class="logo">Web tình duyên</a>
+		<div class="header-right">
+			<a class="active" href="#home">Home</a>
+			<a href="#contact">Contact</a>
+			<a href="#about">About</a>
+		</div>
+	</div>
+
+	<section>
+		<nav>
+			<ul >
+                <li class="menu listmenu" style="background-image: url('image/menubg.jpg');">
+                	Menu
+                </li>
+				<li href="#" class="listmenu">
+					<image src="image/banthan.png" width="50">
+                	<a href="#" style="font-size: 20px">Bản thân</a>
+                </li>
+                <li href="#" class="listmenu">
+					<image src="image/chat.png" width="50">
+                	<a href="#" style="font-size: 20px">Chat</a>
+                </li>
+                <li href="#" class="listmenu">
+					<image src="image/ghepdoi.png" width="50">
+                	<a href="#" style="font-size: 20px">Ghép đôi</a>
+                </li>
+                <li href="#" class="listmenu">
+					<image src="image/thongbao.png" width="50">
+                	<a href="#" style="font-size: 20px">Thông báo</a>
+                </li>
+            </ul>
+		</nav>
+
+		<article>
+			<div class="wrapper" style="float: left;">
+				<div class="profile">
+					<div>
+						<img src="image/dohuong.jpg" class="thumbnail">
+					</div>
+					<h3 class="name">Đỗ Hương</h3>
+					<p class="title">Mô tả chút</p>
+				</div>
+			</div>
+
+			<div class="wrapper" style="float: left;">
+				<div class="profile">
+					<div>
+						<img src="image/talan.jpg" class="thumbnail">
+					</div>
+					
+					<div class="check"><i class="fas fa-check"></i></div>
+					<h3 class="name">Tạ Lan</h3>
+					<p class="title">Javascript Developer</p>
+				</div>
+			</div>
+
+			<div class="wrapper" style="float: left;">
+				<div class="profile">
+					<div>
+						<img src="image/dohuong.jpg" class="thumbnail">
+					</div>
+					
+					<div class="check"><i class="fas fa-check"></i></div>
+					<h3 class="name">Đỗ Hương</h3>
+					<p class="title">Javascript Developer</p>
+				</div>
+			</div>
+			
+			<div class="wrapper" style="float: left;">
+				<div class="profile">
+					<div>
+						<img src="image/talan.jpg" class="thumbnail">
+					</div>
+					
+					<div class="check"><i class="fas fa-check"></i></div>
+					<h3 class="name">Tạ Lan</h3>
+					<p class="title">Javascript Developer</p>
+				</div>
+			</div>
+			
+			<div class="wrapper" style="float: left;">
+				<div class="profile">
+					<div>
+						<img src="image/dohuong.jpg" class="thumbnail">
+					</div>
+					<h3 class="name">Đỗ Hương</h3>
+					<p class="title">Mô tả chút</p>
+				</div>
+			</div>
+
+			<div class="wrapper" style="float: left;">
+				<div class="profile">
+					<div>
+						<img src="image/talan.jpg" class="thumbnail">
+					</div>
+					
+					<div class="check"><i class="fas fa-check"></i></div>
+					<h3 class="name">Tạ Lan</h3>
+					<p class="title">Javascript Developer</p>
+				</div>
+			</div>
+
+			<div class="wrapper" style="float: left;">
+				<div class="profile">
+					<div>
+						<img src="image/dohuong.jpg" class="thumbnail">
+					</div>
+					
+					<div class="check"><i class="fas fa-check"></i></div>
+					<h3 class="name">Đỗ Hương</h3>
+					<p class="title">Javascript Developer</p>
+				</div>
+			</div>
+			
+			<div class="wrapper" style="float: left;">
+				<div class="profile">
+					<div>
+						<img src="image/talan.jpg" class="thumbnail">
+					</div>
+					
+					<div class="check"><i class="fas fa-check"></i></div>
+					<h3 class="name">Tạ Lan</h3>
+					<p class="title">Javascript Developer</p>
+				</div>
+			</div>
+		</article>
+	</section>
+
+</body>
+</html>
